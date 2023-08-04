035532120b794ab5f1411ba667a775f5300280e8
Gulliver <shipulinshshsh2000@gmail.com>
2023-08-05 00:33:26 +0300

    feat: add new version Programm

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -3,7 +3,7 @@
     for (int i = 0; i < mas.Length; i++)
     {
         Console.Write($"Введите {i} элемент начального массива: ");
-        mas[i] = Console.ReadLine();
+        mas[i] = Console.ReadLine()!;
     }
 }
 
@@ -17,24 +17,40 @@ void PrintArray(string[] mas)
     Console.WriteLine("]");
 }
 
+int ColumnSecondMas(string[] mas)
+{
+    int count = 0;
+    for (int i = 0; i < mas.Length; i++)
+    {
+        if (mas[i].Length < 4)
+            count++;
+    }
+    return count;
+}
+
 void FillSecondArray(string[] masOne, string[] masTwo)
 {
-    int elementOneMas;
-    for (int i = 0; i < masTwo.Length; i++)
+    int j = 0;
+    for (int i = 0; i < masOne.Length; i++)
     {
-        elementOneMas = new Random().Next(0, masOne.Length);
-        masTwo[i] = masOne[elementOneMas];
+        if (masOne[i].Length < 4)
+        {
+            masTwo[j] = masOne[i];
+            j++;
+        }
     }
 }
 
 Console.WriteLine("Введите размер начального массива: ");
 int columnFirst = int.Parse(Console.ReadLine()!);
 string[] FirstArray = new string[columnFirst];
-int columnSecond = new Random().Next(0, 4);
-string[] SecondArray = new string[columnSecond];
 FillArray(FirstArray);
 Console.WriteLine("\n Начальный массив");
 PrintArray(FirstArray);
+int columnSecond = ColumnSecondMas(FirstArray);
+string[] SecondArray = new string[columnSecond];
 FillSecondArray(FirstArray, SecondArray);
 Console.WriteLine($"\n Полученный массив с [{columnSecond}] эл.");
+if (columnSecond == 0)
+    return;
 PrintArray(SecondArray);
